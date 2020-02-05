8e5fcf25c06631112c6ab9b708f76707e5e037b6
Paledots <60693561+Paledots@users.noreply.github.com>
2020-02-05 18:27:15 +0300

    Update getSample.py

--- getSample.py
+++ getSample.py
@@ -1,8 +1,8 @@
 f = open('asmc.sample', 'w')
-length = int(input())
+gens = int(input())  # number of genotypes
 
 f.write('ID_1 ID_2 missing\n0 0 0\n')
-for i in range(1, length+1):
+for i in range(1, gens+1):
     f.write('1_' + str(i+1) + ' 1_' + str(i+1) + ' 0\n')
 
 f.close()
